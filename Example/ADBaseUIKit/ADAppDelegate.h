1ac2b76656f0aa2a5d2299bfacd7a61a32516d8c
hongsang <hongsang@2dfire.com>
2019-02-12 12:13:58 +0800

    Initial commit

---
+++ Example/ADBaseUIKit/ADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADAppDelegate.h
+//  ADBaseUIKit
+//
+//  Created by ny8080 on 02/12/2019.
+//  Copyright (c) 2019 ny8080. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ADBaseUIKit/ADViewController.h
@@ -0,0 +1,13 @@
+//
+//  ADViewController.h
+//  ADBaseUIKit
+//
+//  Created by ny8080 on 02/12/2019.
+//  Copyright (c) 2019 ny8080. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ADViewController : UIViewController
+
+@end
